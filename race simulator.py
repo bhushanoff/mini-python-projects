6f74637eaa483f69b695a01ab7534ef14dc4ea6a
bhushanoff <bhushanoffone@gmail.com>
2019-04-25 12:39:52 +0530

    tracks final

--- race simulator.py
+++ race simulator.py
@@ -9,22 +9,27 @@ for i in range (num):
 
 j=0
 for i in range(length//2+num+1):
-    '''
+
     if i<num:
         for j in range(num):
-            print('**')
+            print('  ',end='')
     elif i>=num:
         for j in range(i):  #blank spaces in beginning
-            print('##',end='')     #2 blank spaces
-    '''
+            print('  ',end='')     #2 blank spaces
+
     if i<=length//2 :
         for j in range(num+1):  # for | | .... in beginning
-            if j < num:
+            if i < num and j!=num:
+                print('',end='')
+            elif j<num:
                 print('\\ ',end='')
             else:
                 if i==0:
                     print('iSi',end='')
                 else:
+                    if i<num:
+                        for x in range(i):
+                            print('\\ ',end='')
                     print('|'+str(i%10)+'|',end='')
         
     if i<length:   # blank spaces in middle
@@ -43,10 +48,10 @@ for i in range(length//2+num+1):
             if i==0 :
                 if j==0:
                     print('iFi',end='')
-                else:
-                    print(' /',end='')
             else:
-                if j==0 and i<length//2:
+                if i<num and j>i:
+                    continue
+                elif j==0 and i<length//2:
                     print('|',(length-i)%10,'|',sep='',end='')
                 elif i == length//2 and j==0:
                     print('',end='')
